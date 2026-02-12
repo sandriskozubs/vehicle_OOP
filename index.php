493c6ef690d9d9c160a67fc37beadf0a8e33483c
Sandris <sandrisk32@gmail.com>
2026-02-12 13:23:17 +0200

    Adjusted code

--- index.php
+++ index.php
@@ -10,11 +10,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
     <title>Main</title>
-    <h1>faggot</h1>
 </head>
 <body>
-
-    
 
 </body>
 </html>
